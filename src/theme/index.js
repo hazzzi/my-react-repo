0b9e7157bbd673787c989eaa34ae0eaa874f8514
hazzzi <hanee_l@naver.com>
2021-04-07 18:42:08 +0900

    rename color to palette

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,9 +1,17 @@
-const colors = {
-    main: '#00ABFF',
+const palette = {
+    primary: {
+        light: '#02C7F2',
+        main: '#00ABFF',
+    },
+    text: {
+        main: '#4C4C4C',
+        light: '#9F9F9F',
+    },
+    border: '#E0E0E0',
 }
 
 const theme = {
-    colors,
+    palette,
 }
 
 export default theme
